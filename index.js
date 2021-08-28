bb5d115b19ec2dc80174a8a44ab91545426a4892
Chamon562 <Chamon562@gmail.com>
2021-08-28 16:36:50 -0700

    [.gitignore] Add .env and package-lock.json to .gitignore

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-
+const cors = require("cors")
 app.listen(8318, () =>{
     console.log("Backend server is running");
 })
