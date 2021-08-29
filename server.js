d57fb198a6e277591fdb2750f42bc8164f5bc5d1
Chamon562 <Chamon562@gmail.com>
2021-08-29 12:13:03 -0700

    Add corsOption origin

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors")
+
+let corsOptions = {
+    origin: "http://localhost:8318"
+}
+
+app.use(cors(corsOptions));
+
+
 
 app.listen(8318, () =>{
     console.log("Backend server is running");
