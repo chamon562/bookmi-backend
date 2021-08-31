23e1cf7fd1c948007bc42b96468cc2c8ab7cdb02
Chamon562 <Chamon562@gmail.com>
2021-08-31 16:20:48 -0700

    [role.modle.js] Create role data model

---
+++ app/models/role.model.js
@@ -0,0 +1,11 @@
+// create User and ROle data model 
+const mongoose = require("mongoose");
+
+const Role = mongoose.model(
+    "Role",
+    new mongoose.Schema({
+        name: String
+    })
+);
+
+module.exports = Role;
